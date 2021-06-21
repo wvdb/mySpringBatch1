923c858d1ad0bd59028e3dca223a8163c612e78f
wvdbrande <wimvandenbrande.home@gmail.com>
2021-06-21 14:14:13 +0200

    first commit

--- src/main/java/be/colruyt/e2e/ordermanagement/mySpringBatch1/MySpringBatch1Application.java
+++ src/main/java/be/colruyt/e2e/ordermanagement/mySpringBatch1/MySpringBatch1Application.java
@@ -2,8 +2,10 @@ package be.colruyt.e2e.ordermanagement.mySpringBatch1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"application.properties", "metrics-sample.properties"})
 public class MySpringBatch1Application {
 
 	public static void main(String[] args) {
