8eba79d079f4297555742e1e7a68fbb4a8efdfb3
wvd4f1y <wim.vandenbrande@colruytgroup.com>
2022-04-14 16:31:16 +0200

    Supporting 2 REPOs

---
+++ src/main/java/be/colruyt/e2e/ordermanagement/mySpringBatch1/model/PurchaseRepository.java
@@ -0,0 +1,10 @@
+package be.colruyt.e2e.ordermanagement.mySpringBatch1.model;
+
+/**
+ * Created by Wim Van den Brande on 25/04/2018.
+ *
+ */
+//@RepositoryRestResource(collectionResourceRel = "purchases", path = "purchases")
+//public interface PurchaseRepository extends CrudRepository<Purchase,Integer> {
+//    Purchase findByName(@Param("name") String name);
+//}
