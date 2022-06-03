364a79ea16123254457f395bd2ea384c54e7b940
wvdbrande <wimvandenbrande.home@gmail.com>
2022-06-03 21:28:18 +0200

    Support for rate-limiter

--- src/main/java/be/colruyt/e2e/ordermanagement/mySpringBatch1/controller/MyController.java
+++ src/main/java/be/colruyt/e2e/ordermanagement/mySpringBatch1/controller/MyController.java
@@ -1,8 +1,11 @@
 package be.colruyt.e2e.ordermanagement.mySpringBatch1.controller;
 
 import be.colruyt.e2e.ordermanagement.mySpringBatch1.model.Customer;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +18,16 @@ public class MyController {
     @Autowired
     private Customer customer;
 
-    @GetMapping(value="/dummy",
-            produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Customer> dummy() {
+    @GetMapping(value="/dummy", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RateLimiter(name = "getMessageRateLimit", fallbackMethod = "getMessageFallBack")
+    public ResponseEntity dummy() {
         log.info("Created customer = " + customer);
         return ResponseEntity.ok(customer);
+    }
+
+    public ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+        log.info("Rate limit has applied, So no further calls are getting accepted");
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Too many requests : No further request will be accepted. Please try after sometime");
     }
 
 }
